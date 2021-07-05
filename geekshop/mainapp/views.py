30d584444c1a4d2bfbdc5b4ed035c3ae759b8497
sergey <samael.env@gmail.com>
2021-07-05 15:29:11 +0300

    lesson_4 commit

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -5,7 +5,6 @@ from .models import Product, ProductCategory
 
 # Create your views here.
 def products(request, id=None):
-    print(id)
     title = 'Продукты'
     links_menu = ProductCategory.objects.all()
     products = Product.objects.all()
@@ -18,22 +17,22 @@ def products(request, id=None):
 
 # Заполняем базу из файлов
 def add_productcategory(request):
-    # with open('mainapp/ProductCategory.json') as f:
-    #     templates = json.load(f)
-    # for item in templates['ProductCategory']:
-    #     new_category = ProductCategory(name=item)
-    #     new_category.save()
+    with open('mainapp/ProductCategory.json') as f:
+        templates = json.load(f)
+    for item in templates['ProductCategory']:
+        new_category = ProductCategory(name=item)
+        new_category.save()
     return render(request, 'mainapp/import.html')
 
 def add_product(request):
-    # with open('mainapp/products.json') as f:
-    #     templates = json.load(f)
-    #     for item in templates:
-    #         new_product = Product(name=item['name'],
-    #                               category_id=item['category'],
-    #                               image=item['image'],
-    #                               price=item['price'],
-    #                               quantity=item['quantity'])
-    #         new_product.save()
+    with open('mainapp/products.json') as f:
+        templates = json.load(f)
+        for item in templates:
+            new_product = Product(name=item['name'],
+                                  category_id=item['category'],
+                                  image=item['image'],
+                                  price=item['price'],
+                                  quantity=item['quantity'])
+            new_product.save()
     return render(request, 'mainapp/import.html')
 
